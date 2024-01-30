3d50f2b3f7b355966569636a0999b71e133d68fc
Sameer Himati <sameerhimati98@gmail.com>
2024-01-30 15:02:28 -0600

    Model Stuff

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -6,8 +6,8 @@ from dotenv import find_dotenv, load_dotenv
 
 
 @click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
+@click.argument('data/raw/apple_quality.csv', type=click.Path(exists=True))
+@click.argument('data/processed/apple_quality_cleaned.csv', type=click.Path())
 def main(input_filepath, output_filepath):
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
